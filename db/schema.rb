4031fe5943c687bf7ceb573cbdfb918f2be5e22c
MavisZeng <mayzeng914@hotmail.com>
2014-09-23 14:06:19 -0700

    add session database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920011808) do
+ActiveRecord::Schema.define(version: 20140923025858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20140920011808) do
   add_index "foodiepictures", ["comments_id"], name: "index_foodiepictures_on_comments_id", using: :btree
   add_index "foodiepictures", ["user_id"], name: "index_foodiepictures_on_user_id", using: :btree
 
+  create_table "sessions", force: true do |t|
+    t.integer  "users_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["users_id"], name: "index_sessions_on_users_id", using: :btree
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "password_digest"
@@ -54,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140920011808) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "image_fingerprint"
   end
 
   add_index "users", ["comments_id"], name: "index_users_on_comments_id", using: :btree
